43fba29adb09d40969b550c6b4e7764ad391e9b5
santhu7794 <santhu.vita@gmail.com>
2023-12-11 10:54:29 -0500

    commit

--- script1.js
+++ script1.js
@@ -1,58 +1,57 @@
 
-        function move1() {
-            document.getElementById('11').src = '';
-            document.getElementById('21').src = 'white b.png'
-        }
-        function move() {
-            document.getElementById('1').src = '';
-            document.getElementById('61').src = 'm1.png'
-        }
-        function santhu(){
-        document.getElementById('2').src='';
-       document.getElementById('33').src = 'h1.png'
-    }
-    function m(){
-        document.getElementById('7').src='';
-       document.getElementById('36').src = 'h1.png'
-    }
-    function nn(){
-        document.getElementById('12').src='';
-       document.getElementById('22').src = 'white b.png'
-    }
-    function c(){
-        document.getElementById('14').src='';
-       document.getElementById('34').src = 'white b.png'
-    }
-    function d(){
-        document.getElementById('15').src='';
-       document.getElementById('35').src = 'white b.png'
-    }
-    function mo(){
-        document.getElementById('17').src='';
-       document.getElementById('37').src = 'white b.png'
-    }
-    function mm(){
-        document.getElementById('18').src='';
-       document.getElementById('38').src = 'white b.png'
-    }
-    function p(){
-        document.getElementById('8').src='';
-       document.getElementById('68').src = 'm1.png'
-    }
-    function nani(){
-        document.getElementById('3').src='';
-       document.getElementById('12').src = 'b1.webp'
-    }
-    function nadeem(){
-        document.getElementById('6').src='';
-       document.getElementById('15').src = 'b1.webp'
-    }
-    function script(){
-        document.getElementById('4').src='';
-       document.getElementById('3').src = 'k1.png'
-    }
-    function sa(){
-        document.getElementById('5').src='';
-       document.getElementById('14').src = 'q1.png'
-    }
-   
+function move1() {
+    document.getElementById('11').src = '';
+    document.getElementById('21').src = 'white b.png'
+}
+function move() {
+    document.getElementById('1').src = '';
+    document.getElementById('61').src = 'm1.png'
+}
+function santhu() {
+    document.getElementById('2').src = '';
+    document.getElementById('33').src = 'h1.png'
+}
+function m() {
+    document.getElementById('7').src = '';
+    document.getElementById('36').src = 'h1.png'
+}
+function nn() {
+    document.getElementById('12').src = '';
+    document.getElementById('22').src = 'white b.png'
+}
+function c() {
+    document.getElementById('14').src = '';
+    document.getElementById('34').src = 'white b.png'
+}
+function d() {
+    document.getElementById('15').src = '';
+    document.getElementById('35').src = 'white b.png'
+}
+function mo() {
+    document.getElementById('17').src = '';
+    document.getElementById('37').src = 'white b.png'
+}
+function mm() {
+    document.getElementById('18').src = '';
+    document.getElementById('38').src = 'white b.png'
+}
+function p() {
+    document.getElementById('8').src = '';
+    document.getElementById('68').src = 'm1.png'
+}
+function nani() {
+    document.getElementById('3').src = '';
+    document.getElementById('12').src = 'b1.webp'
+}
+function nadeem() {
+    document.getElementById('6').src = '';
+    document.getElementById('15').src = 'b1.webp'
+}
+function script() {
+    document.getElementById('4').src = '';
+    document.getElementById('3').src = 'k1.png'
+}
+function sa() {
+    document.getElementById('5').src = '';
+    document.getElementById('14').src = 'q1.png'
+}
